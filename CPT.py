0e38736e7b099b51c8223ab6ca28000eff33e395
Stefan Tuczynski <stefanvaldemar.tuczynski23@ycdsbk12.ca>
2021-01-18 11:02:45 -0500

    Commit 1/18/2021

--- CPT.py
+++ CPT.py
@@ -14,14 +14,28 @@ Date:   01/27/2021
 import pygame
  
 # Defined colors
-BLACK          = (   0,   0,   0)
-WHITE          = ( 255, 255, 255)
-GREEN          = (   0, 255,   0)
-RED            = ( 255,   0,   0)
-BUTTON_ONE_COL = (  25, 190, 255)
-HOVER_ONE_COL  = (  75, 225, 225)
-BUTTON_TWO_COL = ( 200,   0,   0)
-HOVER_TWO_COL  = ( 255, 100, 100)
+BLACK            = (   0,   0,   0)
+WHITE            = ( 255, 255, 255)
+GREEN            = (   0, 255,   0)
+RED              = ( 255,   0,   0)
+BUTTON_ONE_COL   = (  25, 190, 255) # Start
+HOVER_ONE_COL    = (  75, 225, 225) # Start
+BUTTON_TWO_COL   = ( 200,   0,   0) # Quit
+HOVER_TWO_COL    = ( 255, 100, 100) # Quit
+BUTTON_THREE_COL = ( 238, 149, 114) # RAM
+HOVER_THREE_COL  = ( 205, 129,  98) # RAM
+BUTTON_FOUR_COL  = (   0, 238,   0) # GPU
+HOVER_FOUR_COL   = (   0, 205,   0) # GPU
+BUTTON_FIVE_COL  = ( 238,  44,  44) # CPU
+HOVER_FIVE_COL   = ( 205,  38,  38) # CPU
+BUTTON_SIX_COL   = ( 151, 255, 255) # PSU
+HOVER_SIX_COL    = ( 121, 205, 205) # PSU
+BUTTON_SEVEN_COL = ( 255, 185,  15) # Storage
+HOVER_SEVEN_COL  = ( 205, 149,  12) # Storage
+BUTTON_EIGHT_COL = ( 255,  20, 147) # Motherboard
+HOVER_EIGHT_COL  = ( 205,  16, 118) # Motherboard
+BUTTON_NINE_COL  = ( 255, 187, 255) # Case
+HOVER_NINE_COL   = ( 205, 150, 205) # Case
 
 # Initiate Pygame
 pygame.init()
@@ -34,26 +48,36 @@ screen = pygame.display.set_mode(size)
 position = pygame.mouse.get_pos()
 click = pygame.mouse.get_pressed()
 
-# Set Graphics
+# Set graphics
 background = pygame.image.load("background.jpg").convert()
 computer = pygame.image.load("computer.png").convert_alpha()
 top_text = pygame.image.load("top_text.png").convert_alpha()
 top_text_two = pygame.image.load("top_text_two.png").convert_alpha()
 info_board = pygame.image.load("info_board.png").convert()
 
+# Set font
+font_one = pygame.font.SysFont('Calibri', 40, True, False)
+font_two = pygame.font.SysFont('Calibri', 30, True, False)
+font_three = pygame.font.SysFont('Calibri', 25, True, False)
+
 # Set text
-font = pygame.font.SysFont('Calibri', 40, True, False)
-text_one = font.render("Start", True, BLACK)
-text_two = font.render("Quit", True, BLACK)
+start_text = font_one.render("Start", True, BLACK)
+quit_text = font_one.render("Quit", True, BLACK)
+ram_text = font_one.render("RAM", True, BLACK)
+gpu_text = font_one.render("GPU", True, BLACK)
+cpu_text = font_one.render("CPU", True, BLACK)
+psu_text = font_one.render("PSU", True, BLACK)
+storage_text = font_two.render("Storage", True, BLACK)
+motherboard_text = font_three.render("Motherboard", True, BLACK)
+case_text = font_one.render("Case", True, BLACK)
 
 # Set position of graphics
 background_position   = [0      ,0]
-computer_position     = [525,  220]
+computer_position     = [250,  300]
 top_text_position     = [300,  -30]
 top_text_two_position = [775,  120]
-text_one_position     = [555,  857]
-text_two_position     = [1160, 857]
 info_board_position   = [400,  300]
+
 # Displays the title for the application
 pygame.display.set_caption("Stefan's CPT")
  
@@ -82,7 +106,7 @@ while not done:
                 if event.type == pygame.MOUSEBUTTONDOWN: # If user clicked the button
                     quit() #Quits the program
 
-    # Drawing code
+    # Draw Main Page
     screen.blit(background, background_position)
     screen.blit(top_text, top_text_position)
     screen.blit(top_text_two, top_text_two_position)
@@ -92,28 +116,104 @@ while not done:
     position = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-    # Logic for Start Button in Menu
+    # Hover Function for Start Button in Menu
     if 500 + 200 > position[0] > 500 and 850 + 50 > position[1] > 850:
         pygame.draw.rect(screen, HOVER_ONE_COL,(500,850,200,50))
-        screen.blit(text_one, text_one_position)
+        screen.blit(start_text, [555, 857])
     else:
         pygame.draw.rect(screen, BUTTON_ONE_COL,(500,850,200,50))
-        screen.blit(text_one, text_one_position)
+        screen.blit(start_text, [555, 857])
 
-    # Logic for Quit Button in Menu
+    # Hover Function for Quit Button in Menu
     if 1100 + 200 > position[0] > 1100 and 850 + 50 > position[1] > 850:
         pygame.draw.rect(screen, HOVER_TWO_COL,(1100,850,200,50))
-        screen.blit(text_two, text_two_position)
+        screen.blit(quit_text, [1160, 857])
     else:
         pygame.draw.rect(screen, BUTTON_TWO_COL,(1100,850,200,50))
-        screen.blit(text_two, text_two_position)
+        screen.blit(quit_text, [1160, 857])
 
+    # Start Game Function
     if start:
         screen.blit(background, background_position)
         screen.blit(top_text, top_text_position)
         screen.blit(top_text_two, top_text_two_position)
         screen.blit(computer, computer_position)
 
+        # Drawing lines and boxes with hover colour changer
+
+        # Ram Button
+        if 525 + 150 > position[0] > 525 and 250 + 50 > position[1] > 250:
+            pygame.draw.line(screen, HOVER_THREE_COL, [600, 300], [500, 460], 5)
+            pygame.draw.rect(screen, HOVER_THREE_COL,(525,250,150,50))
+            screen.blit(ram_text, [554, 256])
+        else:
+            pygame.draw.line(screen, BUTTON_THREE_COL, [600, 300], [500, 460], 5)
+            pygame.draw.rect(screen, BUTTON_THREE_COL,(525,250,150,50))
+            screen.blit(ram_text, [554, 256])
+    
+        # GPU Button
+        if 30 + 150 > position[0] > 30 and 577 + 50 > position[1] > 577:
+            pygame.draw.line(screen, HOVER_FOUR_COL, [170, 600], [340, 610], 5)
+            pygame.draw.line(screen, HOVER_FOUR_COL, [170, 600], [340, 570], 5)
+            pygame.draw.rect(screen, HOVER_FOUR_COL, (30,577,150,50))
+            screen.blit(gpu_text, [65, 585])
+        else:
+            pygame.draw.line(screen, BUTTON_FOUR_COL, [170, 600], [340, 610], 5)
+            pygame.draw.line(screen, BUTTON_FOUR_COL, [170, 600], [340, 570], 5)
+            pygame.draw.rect(screen, BUTTON_FOUR_COL, (30,577,150,50))
+            screen.blit(gpu_text, [65,585])
+
+        # CPU Button
+        if 330 + 150 > position[0] > 330 and 220 + 50 > position[1] > 220:
+            pygame.draw.line(screen, HOVER_FIVE_COL, [400, 230], [430, 480], 5)
+            pygame.draw.rect(screen, HOVER_FIVE_COL, (330,220,150,50))
+            screen.blit(cpu_text, [365,225])
+        else:
+            pygame.draw.line(screen, BUTTON_FIVE_COL, [400, 230], [430, 480], 5)
+            pygame.draw.rect(screen, BUTTON_FIVE_COL, (330,220,150,50))
+            screen.blit(cpu_text, [365,225])
+
+        # PSU Button
+        if 330 + 150 > position[0] > 330 and 800 + 50 > position[1] > 800:
+            pygame.draw.line(screen, HOVER_SIX_COL, [400, 800], [340, 690], 5)
+            pygame.draw.rect(screen, HOVER_SIX_COL, (330,800,150,50))
+            screen.blit(psu_text, [370,810])
+        else:
+            pygame.draw.line(screen, BUTTON_SIX_COL, [400, 800], [340, 690], 5)
+            pygame.draw.rect(screen, BUTTON_SIX_COL, (330,800,150,50))
+            screen.blit(psu_text, [370,810])
+
+        # Storage Button
+        if 800 + 150 > position[0] > 800 and 575 + 50 > position[1] > 575:
+            pygame.draw.line(screen, HOVER_SEVEN_COL, [800, 600], [620, 670], 5)
+            pygame.draw.rect(screen, HOVER_SEVEN_COL, (800,575,150,50))
+            screen.blit(storage_text, [825,585])
+        else:
+            pygame.draw.line(screen, BUTTON_SEVEN_COL, [800, 600], [620, 670], 5)
+            pygame.draw.rect(screen, BUTTON_SEVEN_COL, (800,575,150,50))
+            screen.blit(storage_text, [825,585])
+
+        # Motherboard Button
+        if 20 + 150 > position[0] > 20 and 375 + 50 > position[1] > 375:
+            pygame.draw.line(screen, HOVER_EIGHT_COL, [170, 400], [420, 540], 5)
+            pygame.draw.rect(screen, HOVER_EIGHT_COL, (20,375,150,50))
+            screen.blit(motherboard_text, [25,386])
+
+        else:
+            pygame.draw.line(screen, BUTTON_EIGHT_COL, [170, 400], [420, 540], 5)
+            pygame.draw.rect(screen, BUTTON_EIGHT_COL, (20,375,150,50))
+            screen.blit(motherboard_text, [25,386])
+
+        # Case Button
+        if 800 + 150 > position[0] > 800 and 375 + 50 > position[1] > 375:
+            pygame.draw.line(screen, HOVER_NINE_COL, [800, 400], [620, 550], 5)
+            pygame.draw.rect(screen, HOVER_NINE_COL, (800,375,150,50))
+            screen.blit(case_text, [835,380])
+        else:
+            pygame.draw.line(screen, BUTTON_NINE_COL, [800, 400], [620, 550], 5)
+            pygame.draw.rect(screen, BUTTON_NINE_COL, (800,375,150,50))
+            screen.blit(case_text, [835,380])
+    
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
